d5cd14c2150609ec557ea57f5d6bfaa4f799593f
R1j1t <22280243+R1j1t@users.noreply.github.com>
2024-08-13 13:39:40 -0700

    stored pweres array in a variable

--- chimesFF/src/chimesFF.cpp
+++ chimesFF/src/chimesFF.cpp
@@ -1828,20 +1828,15 @@ void chimesFF::compute_4B(const vector<double> & dx, const vector<double> & dr, 
     int powers[npairs] ;
     double force_scalar[npairs] ;
 
-    // Separte power matrix population of the loop.
-    for(int coeffs=0; coeffs<ncoeffs_4b[quadidx] ; coeffs++)
+    for(int coeffs=0; coeffs<ncoeffs_4b[quadidx]; coeffs++)
     {
         coeff = chimes_4b_params[quadidx][coeffs];
+        vector<int> copowers = chimes_4b_powers[quadidx][coeffs]; 
         
+        // openacc for parallel 
         for (int i=0; i<npairs; i++)
-            powers[i] = chimes_4b_powers[quadidx][coeffs][mapped_pair_idx[i]];
+            powers[i] = copowers[mapped_pair_idx[i]];
 
-    }
-
-    for(int coeffs=0; coeffs<ncoeffs_4b[quadidx] ; coeffs++)
-    {
-        coeff = chimes_4b_params[quadidx][coeffs];
-        
         double Tn_ij_ik_il =  Tn_ij[ powers[0] ] * Tn_ik[ powers[1] ] * Tn_il[ powers[2] ] ;
         double Tn_jk_jl    =  Tn_jk[ powers[3] ] * Tn_jl[ powers[4] ] ;
         double Tn_kl_5     =  Tn_kl[ powers[5] ] ;
