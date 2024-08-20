da7dea06209537a00942e012a722085e76b32432
R1j1t <22280243+R1j1t@users.noreply.github.com>
2024-08-19 19:26:21 -0700

    fixed lammps version and added debug statements

--- etc/lmp/install.sh
+++ etc/lmp/install.sh
@@ -15,21 +15,22 @@ echo ""
 
 # Grab the specific stable branch of LAMMPS compaitbility has been tested for
 
-mkdir -p build/lammps_stable_27Oct2024
+mkdir -p build/lammps_stable_27Jun2024
 
 
-git clone --depth 1 --branch patch_27Jun2024 https://github.com/lammps/lammps.git build/lammps_stable_27Oct2024
+git clone --depth 1 --branch patch_27Jun2024 https://github.com/lammps/lammps.git build/lammps_stable_27Jun2024
 
+echo "LAMMPS stable branch 27Jun2024 cloned"
 
 # Copy ChIMES files to correct locations
 
 # TODO - merge the chimesFF and pair_chimes files for LAMMPS PR
 # TODO - pair.{h,cpp} need to be updated in LAMMPS PR
-cp ../../chimesFF/src/chimesFF.{h,cpp}	build/lammps_stable_27Oct2024/src/MANYBODY/
-cp src/pair_chimes.{h,cpp} 		build/lammps_stable_27Oct2024/src/MANYBODY/
-cp etc/pair.{h,cpp} 			build/lammps_stable_27Oct2024/src
+cp ../../chimesFF/src/chimesFF.{h,cpp}	build/lammps_stable_27Jun2024/src/MANYBODY/
+cp src/pair_chimes.{h,cpp} 		build/lammps_stable_27Jun2024/src/MANYBODY/
+cp etc/pair.{h,cpp} 			build/lammps_stable_27Jun2024/src
 
-
+echo "ChIMES files copied to LAMMPS source"
 
 # Load module files and configure compilers
 # Note: If using intel compilers from after Jan. 2021 (e.g.,) have access to oneapi
@@ -41,7 +42,9 @@ cp etc/pair.{h,cpp} 			build/lammps_stable_27Oct2024/src
 # Compile
 # For GPU build on perlmutter add: -D PKG_KOKKOS=yes -D Kokkos_ARCH_AMPERE80=ON -D Kokkos_ENABLE_CUDA=yes
 
-cd build/lammps_stable_27Oct2024/src
+cd build/lammps_stable_27Jun2024/
+mkdir build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PWD/../install_pm -D CMAKE_BUILD_TYPE=Release \
             -D CMAKE_Fortran_COMPILER=ftn -D CMAKE_C_COMPILER=cc -D CMAKE_CXX_COMPILER=CC \
             -D MPI_C_COMPILER=cc -D MPI_CXX_COMPILER=CC -D LAMMPS_EXCEPTIONS=ON \
@@ -50,18 +53,20 @@ cmake -DCMAKE_INSTALL_PREFIX=$PWD/../install_pm -D CMAKE_BUILD_TYPE=Release \
             -D PKG_RIGID=ON -D PKG_MPIIO=ON \
             -D CMAKE_POSITION_INDEPENDENT_CODE=ON -D CMAKE_EXE_FLAGS="-dynamic" ../cmake
 
+echo "Compiling LAMMPS with ChIMES support"
+
 make -j16
 
 # Finish
 
 mkdir exe
-mv build/lammps_stable_27Oct2024/src/lmp_mpi_chimes exe
+mv build/lammps_stable_27Jun2024/build/lmp exe
 
 loc=`pwd`
 echo ""
 echo "Compilation complete. "
 echo "Generated the following LAMMPS executable with ChIMES support:"
-echo "${loc}/exe/lmp_mpi_chimes"
+echo "${loc}/exe/lmp"
 echo "See ${loc}/tests for usage examples"
 echo ""
 
